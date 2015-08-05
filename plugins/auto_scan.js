fe602a13aaf937128e3022bfbf13b47e10929ab7
Carlos Rodriguez <carlos@s8f.org>
2015-08-05 12:17:32 -0700

    fix autoscan at 5 minutes

--- plugins/auto_scan.js
+++ plugins/auto_scan.js
@@ -1,13 +1,10 @@
 module.exports = function (app) {
-  var i;
-  app.on('ready', function () {
-    clearInterval(i);
-    i = setInterval(function () {
-      console.log('scan all');
-      app.scan_all(function (err) {
-        console.log('scan all done');
-        if (err) app.emit('error', err);
-      });
-    }, 60000);
-  });
+  if (app._i) clearInterval(app._i);
+  app._i = setInterval(function () {
+    console.log('scan all');
+    app.scan_all(function (err) {
+      console.log('scan all done');
+      if (err) app.emit('error', err);
+    });
+  }, 60000 * 5);
 };
