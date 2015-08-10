da59b0674b65ecc07f0f80c8116ff068b37a9fdc
Carlos Rodriguez <carlos@s8f.org>
2015-08-10 14:57:27 -0700

    wip on graphing

--- collections/servers.js
+++ collections/servers.js
@@ -1,9 +1,12 @@
+var async = require('async');
+
 module.exports = function (app) {
-  app.require('collection', 'server_posts');
+  app.require('collection', 'server_posts', 'stats');
   return app.collection({
     name: 'servers',
     save: function (server, cb) {
       delete server.last_post;
+      delete server.meta;
       if (!server.label) return cb(new Error('label is required'));
       cb();
     },
@@ -11,7 +14,25 @@ module.exports = function (app) {
       app.server_posts(server.id).tail(0, {load: true}, function (err, chunk, getNext) {
         if (err) return cb(err);
         server.last_post = chunk[0];
-        cb(null, server);
+        server.meta = {};
+        async.parallel({
+          mem_used: function (done) {
+            app.stats.graph(server.id + ':mem_used', '5m', {start: new Date().getTime() - 86400000}, function (err, results) {
+              if (err) return done(err);
+              results = results.map(function (r) {
+                return r.avg;
+              });
+              done(null, results);
+            });
+          }
+        }, function (err, results) {
+          if (err) return cb(err);
+          Object.keys(results).forEach(function (k) {
+            server.meta[k] = results[k].join(',');
+          });
+          console.log('loaded', JSON.stringify(server, null, 2));
+          cb(null, server);
+        });
       });
     }
   });
